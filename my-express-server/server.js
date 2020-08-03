353ac07e26238c8798bb0f7f3c028bae7059a16c
FreyaXiang <xiangxin0511@gmail.com>
2020-08-03 11:45:01 +0800

    add express server and calculator

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const app = express();
 
-app.get("/", function(request, response) {
-  response.send("HELLO");
+app.get("/", (req, res) => {
+  res.send("<h1>HELLO</h1>");
 });
 
-app.listen(3000, function() {
+app.get("/contact", (req, res) => {
+  res.send("<h1>xiangxin0511@gmail.com</h1>");
+});
+
+app.get("/about", (req, res) => {
+  res.send("<h1>My name is Xin Xiang.</h1>");
+});
+
+app.listen(3000, () => {
   console.log("server started on port 3000");
 });
